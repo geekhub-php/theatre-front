4d3319c2e9ebac8748fc8ecbfb053b97942e218f
Serhii Polishchuk <spolishchuk@oroinc.com>
2018-02-22 23:20:38 +0700

    Fixed CS, remove debug code

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -4,10 +4,6 @@ export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
-
-  getMainMenu() {
-    return element(by.css('app-root app-main-menu nav'));
-  }
 }
 
 export class MainMenu {
@@ -20,13 +16,6 @@ export class MainMenu {
   }
 
   private getLink(text) {
-      // this.navEl.all(by.css('a')).then(function(elements: Array<ElementFinder>) {
-      //     elements.forEach(function(el: ElementFinder) {
-      //         el.getText().then(function(elText: string) {
-      //             console.log(elText);
-      //         });
-      //     });
-      // });
       return this.navEl.element(by.cssContainingText('a', text));
   }
 }
