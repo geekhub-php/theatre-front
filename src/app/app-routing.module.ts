a3508127f1b88b08166bbca05f16b5d342f415f4
Serhii Polishchuk <spolishchuk@oroinc.com>
2018-02-22 12:59:15 +0700

    Added routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {PerformanceListComponent} from './performance-list/performance-list.component';
+import {ContactsComponent} from './contacts/contacts.component';
+import {TeamComponent} from './team/team.component';
+import {NewsListComponent} from './news-list/news-list.component';
+import {TeamListComponent} from './team-list/team-list.component';
+import {PerformanceComponent} from './performance/performance.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {NewsComponent} from './news/news.component';
+import {AboutComponent} from './about/about.component';
+import {ScheduleComponent} from './schedule/schedule.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomePageComponent},
+    {path: 'poster', component: ScheduleComponent},
+    {path: 'repertoire', component: PerformanceListComponent},
+    {path: 'performance/:slug', component: PerformanceComponent},
+    {path: 'news', component: NewsListComponent},
+    {path: 'news/:slug', component: NewsComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'persons', component: TeamListComponent},
+    {path: 'persons/:slug', component: TeamComponent},
+    {path: 'contacts', component: ContactsComponent},
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
+  ],
+  exports: [
+      RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,20 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { TeamListComponent } from './team-list/team-list.component';
 import { TeamComponent } from './team/team.component';
 import { ContactsComponent } from './contacts/contacts.component';
-
-const appRoutes: Routes = [
-    {path: '', component: HomePageComponent},
-    {path: 'poster', component: ScheduleComponent},
-    {path: 'repertoire', component: PerformanceListComponent},
-    {path: 'performance/:slug', component: PerformanceComponent},
-    {path: 'news', component: NewsListComponent},
-    {path: 'news/:slug', component: NewsComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'persons', component: TeamListComponent},
-    {path: 'persons/:slug', component: TeamComponent},
-    {path: 'contacts', component: ContactsComponent},
-];
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -46,7 +33,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
