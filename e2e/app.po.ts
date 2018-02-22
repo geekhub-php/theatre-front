9dd596af4d7c29c17d3d245948e185e7823f77a5
Serhii Polishchuk <spolishchuk@oroinc.com>
2018-02-22 23:07:27 +0700

    Fixed protractor tests

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,32 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element, ElementFinder} from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getMainMenu() {
+    return element(by.css('app-root app-main-menu nav'));
+  }
+}
+
+export class MainMenu {
+  navEl = element(by.css('app-root app-main-menu nav'));
+
+  navigateTo(text) {
+    const link = this.getLink(text);
+    expect(link.isPresent).toBeTruthy();
+    link.click();
+  }
+
+  private getLink(text) {
+      // this.navEl.all(by.css('a')).then(function(elements: Array<ElementFinder>) {
+      //     elements.forEach(function(el: ElementFinder) {
+      //         el.getText().then(function(elText: string) {
+      //             console.log(elText);
+      //         });
+      //     });
+      // });
+      return this.navEl.element(by.cssContainingText('a', text));
   }
 }
