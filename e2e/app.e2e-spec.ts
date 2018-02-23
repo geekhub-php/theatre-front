b7b66fa0b444fb8e2246902f0c8d245b4dc34c8a
Serhii Polishchuk <spolishchuk@oroinc.com>
2018-02-23 18:56:30 +0700

    Fixed e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -12,43 +12,43 @@ describe('theatre-front App', () => {
 
   it('shold navigate to Home', () => {
     page.navigateTo();
-    mainMenu.navigateTo('Home');
+    mainMenu.navigateTo('HOME');
     expect(element(by.css('p')).getText()).toContain('home-page works');
   });
 
   it('shold navigate to Poster', () => {
     page.navigateTo();
-    mainMenu.navigateTo('Poster');
+    mainMenu.navigateTo('POSTER');
     expect(element(by.css('p')).getText()).toContain('schedule works');
   });
 
   it('shold navigate to Repertoire', () => {
     page.navigateTo();
-    mainMenu.navigateTo('Repertoire');
+    mainMenu.navigateTo('REPERTOIRE');
     expect(element(by.css('p')).getText()).toContain('performance-list works');
   });
 
   it('shold navigate to News', () => {
     page.navigateTo();
-    mainMenu.navigateTo('News');
+    mainMenu.navigateTo('NEWS');
     expect(element(by.css('p')).getText()).toContain('news-list works');
   });
 
   it('shold navigate to About Us', () => {
     page.navigateTo();
-    mainMenu.navigateTo('About Us');
+    mainMenu.navigateTo('ABOUT US');
     expect(element(by.css('p')).getText()).toContain('about works');
   });
 
   it('shold navigate to Persons', () => {
     page.navigateTo();
-    mainMenu.navigateTo('Persons');
+    mainMenu.navigateTo('PERSONS');
     expect(element(by.css('p')).getText()).toContain('team-list works');
   });
 
   it('shold navigate to Contacts', () => {
     page.navigateTo();
-    mainMenu.navigateTo('Contacts');
+    mainMenu.navigateTo('CONTACTS');
     expect(element(by.css('p')).getText()).toContain('contacts works');
   });
 });
