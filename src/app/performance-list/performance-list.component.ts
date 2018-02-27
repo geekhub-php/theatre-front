59b321d3efd7759503bc7cb90c9acd607f53a9f9
Serhii Polishchuk <spolishchuk@oroinc.com>
2018-02-27 18:53:39 +0700

    Get repertoire from API and show it on the page

--- src/app/performance-list/performance-list.component.ts
+++ src/app/performance-list/performance-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PerformanceRepositoryService} from '../Repository/performance-repository.service';
+import {Performance} from '../models';
 
 @Component({
   selector: 'app-performance-list',
@@ -10,9 +11,21 @@ import {PerformanceRepositoryService} from '../Repository/performance-repository
   ]
 })
 export class PerformanceListComponent implements OnInit {
+  public performances: Performance[];
   constructor(private performanceRepository: PerformanceRepositoryService) { }
 
   ngOnInit() {
+    this.performanceRepository.getList().subscribe(data => this.performances = data.performances);
   }
 
+  onLoad(isSuccess, div) {
+    if (isSuccess) {
+        div.style.height = '100%';
+        div.classList.remove('stub');
+        console.dir(div);
+    } else {
+      // ToDo: add fallback image to src
+      console.log('Error while loading image');
+    }
+  }
 }
