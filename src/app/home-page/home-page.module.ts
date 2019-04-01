2b1239ff71df54782e67f3d5676617ba0ae63ad2
InnaLincoln <42704341+InnaLincoln@users.noreply.github.com>
2019-04-01 15:50:16 +0300

    Home page (#16)
    * added elementary realization
    
    * added routing for left navigation
    
    * some changes
    
    * added two components
    
    * added adaptability
    created partners & broad-trustees component
    
    * fixed adaptability
    
    * deleted needles styles
    
    * fixed json?
    
    * added pictures for left nav
    
    * changed order for mobile version & changed icons
    
    * fixed for tests
    
    * changed rem -> px
    partners & boarding-trustees located in app
    deleted container for slider
    
    * added new format displaying svg
    
    * some changes in news
    
    * some changes
    
    * deleted displaying side-menu on mobile devices
    
    * deleted needless export & fixed README

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-import { HomePageComponent } from './home-page.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { SliderComponent } from './slider/slider.component';
-
+import { NewsListModule } from '../news-list/news-list.module';
 
 @NgModule({
   declarations: [
-    HomePageComponent,
     SideMenuComponent,
     SliderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    NewsListModule
+  ],
+  exports: [
+    SliderComponent,
+    SideMenuComponent
   ]
 })
 export class HomePageModule { }
