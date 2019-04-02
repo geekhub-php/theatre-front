930bd1301da242b45390f25600ef0a9594d5c1d7
Artem Honcharenko <44007616+gonchar44@users.noreply.github.com>
2019-04-02 16:37:29 +0300

    Updated ListResponseInterface.ts (#34)

--- src/app/core/model/PerformanceListResponse.ts
+++ src/app/core/model/PerformanceListResponse.ts
@@ -2,6 +2,8 @@ import { ListResponseInterface } from './ListResponseInterface';
 import { Performance } from './Performance';
 
 export class PerformanceListResponse implements ListResponseInterface {
+  total_count: number;
+  count: number;
   performances: Array<Performance>;
   page: number;
 }
