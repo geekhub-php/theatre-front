b620467fa61eb3339bacbc53bf626a35e9de6113
Artem Honcharenko <artemgonchar7@gmail.com>
2019-04-02 18:07:35 +0300

    Added getEmployees method

--- src/app/core/service/gateway.service.ts
+++ src/app/core/service/gateway.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { PerformanceListResponse } from '../model/PerformanceListResponse';
 import { environment } from '../../../environments/environment';
+import { EmployeesListResponse } from '../model/EmployeesListResponse';
 
 
 @Injectable({
@@ -15,6 +16,12 @@ export class GatewayService {
   readonly performanceListUrl = '/performances.json';
   readonly baseUrl = environment.baseUrl;
 
+  protected httpOptions = {
+    headers: new HttpHeaders(),
+    observe: 'response' as 'body',
+    params: new HttpParams()
+  };
+
   constructor(private http: HttpClient) { }
 
   getPerformanceList(limit = 10): Observable<PerformanceListResponse> {
@@ -22,6 +29,12 @@ export class GatewayService {
       .pipe(
           catchError(this.handleError('get list of Performances', new PerformanceListResponse()))
       );
+  }
+
+  getEmployees(): Observable<HttpResponse<EmployeesListResponse>> {
+    return this.http.get<HttpResponse<EmployeesListResponse>>(
+      this.baseUrl + '/employees.json', this.httpOptions
+    );
   }
 
   /* tslint:disable:no-console */
