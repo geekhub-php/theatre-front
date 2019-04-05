bf0c2b672dac92bd45bfc7174d04d95514d31191
Artem Honcharenko <artemgonchar7@gmail.com>
2019-04-05 19:45:23 +0300

    Added person router

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GatewayService } from '../core/service/gateway.service';
+import { Employee } from '../core/model/Employee';
 
 @Component({
   selector: 'app-person',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonComponent implements OnInit {
 
-  constructor() { }
+  person: Employee;
+  gallery: any;
+
+  constructor(
+    private router: ActivatedRoute,
+    private gatewayService: GatewayService
+  ) { }
 
   ngOnInit() {
+    this.get();
+  }
+
+  get() {
+    const slug = this.router.snapshot.paramMap.get('slug');
+    console.log(slug);
+    this.gatewayService.getEmployees(slug).subscribe((res) => {
+      this.person = res.body.employees.find(employee => employee.slug === slug);
+      console.log(this.person);
+      this.gallery = this.person.gallery;
+      console.log(res.body);
+      console.log(this.gallery);
+    });
   }
 
 }
