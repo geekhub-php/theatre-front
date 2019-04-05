a8df5649e2329444291bc6c04a51d8b5ba547617
Irina Kaban <irina.kaban7@gmail.com>
2019-04-05 22:04:30 +0300

    Fixed slider

--- src/app/performance/performance-slider/performance-slider.component.ts
+++ src/app/performance/performance-slider/performance-slider.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-performance-slider',
   templateUrl: './performance-slider.component.html',
+  styleUrls: ['./performance-slider.component.scss'],
   providers: [NgbCarouselConfig]
 })
 export class PerformanceSliderComponent {
+  showNavigationArrows = true;
   images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+  constructor() {}
 }
