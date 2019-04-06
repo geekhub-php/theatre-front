dc7c263623a94c0a905b2e6937484459abdc9b53
Serhii Polishchuk <spolischook@gmail.com>
2019-04-06 14:09:18 +0300

    Fixed tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -12,43 +12,43 @@ describe('theatre-front App', () => {
 
   it('shold navigate to Home', () => {
     page.navigateTo();
-    mainMenu.navigateTo('HOME');
+    mainMenu.navigateTo('Головна');
     expect(element(by.css('h3')).getText()).toContain('app-home-page');
   });
 
   it('shold navigate to Poster', () => {
     page.navigateTo();
-    mainMenu.navigateTo('POSTER');
+    mainMenu.navigateTo('Афіша');
     expect(element(by.css('p')).getText()).toContain('schedule works');
   });
 
   it('shold navigate to Repertoire', () => {
     page.navigateTo();
-    mainMenu.navigateTo('REPERTOIRE');
+    mainMenu.navigateTo('Репертуар');
     expect(element(by.css('h1')).getText()).toContain('Repertoire');
   });
 
   it('shold navigate to News', () => {
     page.navigateTo();
-    mainMenu.navigateTo('NEWS');
+    mainMenu.navigateTo('Новини');
     expect(element(by.css('p')).getText()).toContain('news-list works');
   });
 
   it('shold navigate to About Us', () => {
     page.navigateTo();
-    mainMenu.navigateTo('ABOUT US');
+    mainMenu.navigateTo('Про театр');
     expect(element(by.css('p')).getText()).toContain('about works');
   });
 
   it('shold navigate to Persons', () => {
     page.navigateTo();
-    mainMenu.navigateTo('PERSONS');
+    mainMenu.navigateTo('Персоналії');
     expect(element(by.css('p')).getText()).toContain('team-list works');
   });
 
   it('shold navigate to Contacts', () => {
     page.navigateTo();
-    mainMenu.navigateTo('CONTACTS');
+    mainMenu.navigateTo('Контакти');
     expect(element(by.css('p')).getText()).toContain('contacts works');
   });
 });
