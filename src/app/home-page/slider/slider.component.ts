b6b5bebe5b9aa86f6c544b6cd3cc191a912b409a
yana-tsulya <yanatsulya@gmail.com>
2019-04-06 13:38:03 +0300

    Active slide change added

--- src/app/home-page/slider/slider.component.ts
+++ src/app/home-page/slider/slider.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
+  activeSlide = 0;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  setActiveSlide(idx: number) {
+    this.activeSlide = idx;
+  }
 }
