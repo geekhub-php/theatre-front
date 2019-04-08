c79a9cf9be31413ad51cc8a086609a92cb71ca2c
RevskyAndrey <andrey.g1362@gmail.com>
2019-04-08 20:10:18 +0300

    update *fix test

--- src/app/performance/performance.component.ts
+++ src/app/performance/performance.component.ts
@@ -8,8 +8,8 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrls: ['./performance.component.scss']
 })
 export class PerformanceComponent implements OnInit {
-  showNavigationArrows = true;
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  // showNavigationArrows = true;
+  // images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
   constructor() {
   }
