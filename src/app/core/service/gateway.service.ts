e0701080cc23b7e63a7ce8ba0c57d3d79dce56e7
RevskyAndrey <andrey.g1362@gmail.com>
2019-04-09 20:25:19 +0300

    сделал запрос, поломал e2e тесты

--- src/app/core/service/gateway.service.ts
+++ src/app/core/service/gateway.service.ts
@@ -12,26 +12,47 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root'
 })
 export class GatewayService {
-  readonly performanceListUrl = '/performances.json';
+  readonly performanceListUrl = 'performances.json';
+  readonly performanceSlugUrl = 'performances/dorogha-do-sontsia.json?locale=uk'; //
+  readonly performanceRolesUrl = 'dorogha-do-sontsia/roles.json?locale=uk'; //
   readonly historiesListUrl = 'histories.json';
   readonly baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getPerformanceList(limit = 10): Observable<PerformanceListResponse> {
-    return this.http.get<PerformanceListResponse>(`${this.baseUrl}/${this.performanceListUrl}`, { params: {limit: '100'} })
+    return this.http.get<PerformanceListResponse>(`${this.baseUrl}/${this.performanceListUrl}`, {params: {limit: '100'}})
       .pipe(
-          catchError(this.handleError('get list of Performances', new PerformanceListResponse()))
+        catchError(this.handleError('get list of Performances', new PerformanceListResponse()))
       );
   }
 
+  //  свою модель нужно написать
+  // ERROR in src/app/performance/performance.component.ts(28,7): error TS2740: Type 'PerformanceListResponse'
+  // is missing the following properties from type 'Performance[]': length, pop, push, concat, and 26 more.
+
+  getPerformanceSlug(limit = 10): Observable<PerformanceListResponse> {
+    return this.http.get<PerformanceListResponse>(`${this.baseUrl}/${this.performanceSlugUrl}`, {params: {limit: '100'}})
+      .pipe(
+        catchError(this.handleError('get list of Performances', new PerformanceListResponse()))
+      );
+  }
+  //  заготовка для запроса на актеров. Нужна модель
+  // getPerformanceRoles(limit = 10): Observable<PerformanceListResponse> {
+  //   return this.http.get<PerformanceListResponse>(`${this.baseUrl}/${this.performanceRolesUrl}`, {params: {limit: '100'}})
+  //     .pipe(
+  //       catchError(this.handleError('get list of Performances', new PerformanceListResponse()))
+  //     );
+  // }
+
   getHistoriesList(limit: string = '10', page: string = '1', locale: string = 'uk'): Observable<HistoryListResponse> {
     return this.http.get<HistoryListResponse>(`${this.baseUrl}/${this.historiesListUrl}`, {
-      params: { limit: limit, page: page, locale: locale }
+      params: {limit: limit, page: page, locale: locale}
     })
-    .pipe(
-      catchError(this.handleError('get list of Histories', new HistoryListResponse()))
-    );
+      .pipe(
+        catchError(this.handleError('get list of Histories', new HistoryListResponse()))
+      );
   }
 
   /* tslint:disable:no-console */
--- src/app/performance/performance.component.ts
+++ src/app/performance/performance.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { GatewayService } from '../core/service/gateway.service';
+import { Performance } from '../core/model/Performance';
+import { PerformanceListResponse } from '../core/model/PerformanceListResponse';
+
 
 @Component({
   selector: 'app-performance',
@@ -9,13 +13,28 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class PerformanceComponent implements OnInit {
   showNavigationArrows = true;
-   images = ['http://api.theatre.pp.ua/uploads/slider/0001/01/thumb_248_slider_slider.jpeg',
-     'http://api.theatre.pp.ua/uploads/slider/0001/01/thumb_248_slider_slider.jpeg'];
-  constructor() {
+
+  listPerfomance: Array<Performance>;
+  // roles: Array<>
+
+  /*  this.gallery.reference.url   тут же и альт для изображения   */
+  images = ['http://api.theatre.pp.ua/uploads/slider/0001/01/thumb_248_slider_slider.jpeg',
+    'http://api.theatre.pp.ua/uploads/slider/0001/01/thumb_248_slider_slider.jpeg'];
+
+  /**/
+  constructor(private getway: GatewayService) {
   }
 
   ngOnInit() {
-    console.dir ('http://api.theatre.pp.ua/performances/dorogha-do-sontsia.json?locale=uk');
-  }
+    this.getway.getPerformanceSlug().subscribe((res: PerformanceListResponse) => {
+      this.listPerfomance = res;
+      console.dir(this.listPerfomance);
+    });
+    //  без модели ни куда
+    // this.getway.getPerformanceRoles().subscribe((res: PerformanceListResponse) => {
+    //   this.roles = res;
+    //   console.dir(this.roles);
+    // });
 
+  }
 }
