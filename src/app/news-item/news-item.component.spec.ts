e58c60d04f31ab74777561dd829996f19818b9a6
Serhii Polishchuk <spolischook@gmail.com>
2019-04-10 20:23:14 +0300

    Fixed one test broke another

--- src/app/news-item/news-item.component.spec.ts
+++ src/app/news-item/news-item.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewsItemComponent } from './news-item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NewsItemComponent', () => {
   let component: NewsItemComponent;
@@ -13,7 +14,9 @@ describe('NewsItemComponent', () => {
       declarations: [ NewsItemComponent ],
       imports: [
         HttpClientModule,
-        RouterModule]
+        RouterModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
