f5e2a7e825c620d74b9a179252c1e54051b2b26e
Serhii Polishchuk <spolischook@gmail.com>
2019-04-10 20:30:38 +0300

    Fixed tests

--- src/app/news-item/news-item.component.spec.ts
+++ src/app/news-item/news-item.component.spec.ts
@@ -4,6 +4,9 @@ import { NewsItemComponent } from './news-item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NewsItem } from '../core/model/news/NewsItem';
+import { NewsItemImage } from '../core/model/news/NewsItemImage';
+import { Image } from '../core/model/Image';
 
 describe('NewsItemComponent', () => {
   let component: NewsItemComponent;
@@ -24,6 +27,9 @@ describe('NewsItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NewsItemComponent);
     component = fixture.componentInstance;
+    component.item = new NewsItem();
+    component.item.mainPicture = new NewsItemImage();
+    component.item.mainPicture.post_big = new Image();
     fixture.detectChanges();
   });
 
