90e419259687fffe63b15bcc41b2f42223f019aa
yana-tsulya <yanatsulya@gmail.com>
2019-04-11 10:52:39 +0300

    Fix gallery package setup

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GatewayService } from '../core/service/gateway.service';
 import { Employee } from '../core/model/Employee';
+import { NgxGalleryOptions, NgxGalleryImage } from 'ngx-gallery';
+
 
 @Component({
   selector: 'app-person',
@@ -9,9 +11,9 @@ import { Employee } from '../core/model/Employee';
   styleUrls: ['./person.component.scss']
 })
 export class PersonComponent implements OnInit {
-
   person: Employee;
-  gallery: any;
+  gallery: Array<NgxGalleryImage> = [];
+  galleryOptions: Array<NgxGalleryOptions>;
 
   constructor(
     private router: ActivatedRoute,
@@ -20,13 +22,35 @@ export class PersonComponent implements OnInit {
 
   ngOnInit() {
     this.getPerson();
+
+    this.galleryOptions = [
+      {
+        image: false,
+        thumbnailsRemainingCount: true,
+        height: '100px',
+        previewCloseOnClick: true,
+        previewCloseOnEsc: true
+      },
+      {
+        breakpoint: 500,
+        width: '100%',
+        thumbnailsColumns: 2,
+        imageSize: 'cover'
+      }
+    ];
   }
 
   getPerson() {
     const slug = this.router.snapshot.paramMap.get('slug');
     this.gatewayService.getEmployeeBySlug(slug).subscribe((res) => {
       this.person = res.body;
-      this.gallery = res.body.gallery;
+      this.gallery = res.body.gallery.map(({images}) => {
+        return {
+          small: images.performance_small.url,
+          medium: images.reference.url,
+          big: images.performance_big.url
+        };
+      });
     });
   }
 }
