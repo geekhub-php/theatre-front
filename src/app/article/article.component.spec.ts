1e2c93d4ca06073a135e9c5cf0ec1d6053c732e9
Inna Ivchenko <i_lincoln_a@list.ru>
2019-04-13 09:27:03 +0300

    fixed tests

--- src/app/article/article.component.spec.ts
+++ src/app/article/article.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArticleComponent } from './article.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StripHtmlModule } from '../shared/pipes/strip-html/strip-html.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ArticleComponent', () => {
   let component: ArticleComponent;
@@ -8,7 +11,8 @@ describe('ArticleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ArticleComponent ]
+      declarations: [ ArticleComponent ],
+      imports: [HttpClientModule, StripHtmlModule, RouterTestingModule]
     })
     .compileComponents();
   }));
--- src/app/shared/pipes/strip-html/strip-html.module.ts
+++ src/app/shared/pipes/strip-html/strip-html.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StripHtmlPipe } from './stripHtml.pipe';
+import { StripNbspPipe } from '../strip-nbsp/strip-nbsp.pipe';
 
 @NgModule({
   declarations: [
-    StripHtmlPipe
+    StripHtmlPipe,
+    StripNbspPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    StripHtmlPipe
+    StripHtmlPipe,
+    StripNbspPipe
   ]
 })
 export class StripHtmlModule { }
