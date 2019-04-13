9744628b5a3c1f001464dc7df910744bc2e2cebc
Artem Honcharenko <artemgonchar7@gmail.com>
2019-04-13 19:57:51 +0300

    fixed template

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
-  templateUrl: './spinner.component.html',
+  template: `
+    <div class="spinner-grow" *ngIf="inProgress" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    `,
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnInit {
