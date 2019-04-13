b562171d9a211f0b2f1d6a83e93b443895d2fb1c
Artem Honcharenko <artemgonchar7@gmail.com>
2019-04-13 20:34:50 +0300

    Added methods

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -11,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpinnerComponent implements OnInit {
 
+  inProgress: boolean;
+
+  @Output() show: EventEmitter<string> = new EventEmitter();
+  @Output() hide: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  showSpinner() {
+    if (this.inProgress === true) this.show.emit('in progress');
+  }
+  hideSpinner() {
+    if (this.inProgress === false) this.hide.emit('finished');
+  }
 }
