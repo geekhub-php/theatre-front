a90c8e7f8f5f08ebde07022cf609dafec0b9d25a
IlyaBielov <ibioriginalibi@gmail.com>
2019-04-14 20:37:54 +0300

    fix test

--- src/app/core/model/widget/WidgetResType.ts
+++ src/app/core/model/widget/WidgetResType.ts
@@ -1,6 +1,6 @@
-export type WidgetResType = {
-    fromDate: string 
-    limit: string 
-    locale: string 
-    performance?: string
+export interface WidgetResType {
+    fromDate: string;
+    limit: string;
+    locale: string;
+    performance?: string;
 }
