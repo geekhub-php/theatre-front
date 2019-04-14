e09aaafeeb4ab99f61543c49e200257ca7510c3e
Serhii Polishchuk <spolischook@gmail.com>
2019-04-14 22:15:53 +0300

    Updated spiner and add it to about component

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,20 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { LoaderService } from './loader.service';
 
 @Component({
   selector: 'app-spinner',
   template: `
-    <div class="spinner-grow" [style.display]='visible ? "block" : "none"' role="status">
-      <span class="sr-only">Loading...</span>
+    <div class="d-flex justify-content-center">
+      <div class="spinner-grow text-danger text-center" [style.display]='visible ? "block" : "none"' role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
     </div>
     `
 })
-export class SpinnerComponent {
+export class SpinnerComponent implements OnInit {
   visible = false;
   @Input() name: string;
 
   constructor(private loaderService: LoaderService) {
+  }
+
+  ngOnInit(): void {
     this.loaderService.subject.subscribe((value: {load: boolean, name: string}) => {
       if (this.name !== value.name) return;
       this.visible = value.load;
