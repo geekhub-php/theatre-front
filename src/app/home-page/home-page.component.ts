37cf8111f458d982c871f3dc49f40888b22dab05
Artem Honcharenko <44007616+gonchar44@users.noreply.github.com>
2019-04-14 22:27:38 +0300

    Created spinner (#74)
    * Created spinner
    
    * fixed template
    
    * Added methods
    
    * Got name
    
    * Implement Spinner loader
    
    * Fixed tests
    
    * Cleaned branch
    
    * Updated spiner and add it to about component
    
    * Fixed tests

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { LoaderService } from '../shared/spinner/loader.service';
 
 @Component({
   selector: 'app-home-page',
