63609ae197dcd06de0792961f25637a026899950
Inna Ivchenko <i_lincoln_a@list.ru>
2019-04-15 12:46:19 +0300

    added spinner

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NewsItem } from '../core/model/news/NewsItem';
 import { ActivatedRoute } from '@angular/router';
 import { GatewayService } from '../core/service/gateway.service';
+import { LoaderService } from '../shared/spinner/loader.service';
 
 @Component({
   selector: 'app-article',
@@ -12,7 +13,9 @@ export class ArticleComponent implements OnInit {
   item: NewsItem;
   posts: Array<NewsItem>;
 
-  constructor(private router: ActivatedRoute, private gateAway: GatewayService) {
+  constructor(private router: ActivatedRoute,
+              private gateAway: GatewayService,
+              private loaderService: LoaderService) {
   }
 
   ngOnInit() {
@@ -20,9 +23,14 @@ export class ArticleComponent implements OnInit {
   }
 
   getArticle() {
+    this.loaderService.start('article');
     const slug = this.router.snapshot.paramMap.get('slug');
     this.gateAway.getNewsBySlug(slug).subscribe((res) => {
       this.item = res.body;
-    });
+        this.loaderService.stop('article');
+      },
+      err => this.loaderService.stop('article')
+    );
+    this.loaderService.start('article');
   }
 }
