074e06c1bfa034b0c7a932b8759b09d573c5da88
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-04-15 22:39:18 +0300

    Added changeDetector.

--- src/app/performance-list/performance-list.component.ts
+++ src/app/performance-list/performance-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
 import { GatewayService } from '../core/service/gateway.service';
 import { PerformanceListResponse } from '../core/model/PerformanceListResponse';
 import { Performance } from '../core/model/Performance';
@@ -6,20 +7,22 @@ import { Performance } from '../core/model/Performance';
 @Component({
   selector: 'app-performance-list',
   templateUrl: './performance-list.component.html',
-  styleUrls: ['./performance-list.component.scss']
+  styleUrls: ['./performance-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PerformanceListComponent implements OnInit {
   response: PerformanceListResponse;
   perfomances: Array<Performance>;
 
-  constructor(private gateway: GatewayService) {
-
-  }
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+    private gateway: GatewayService
+  ) {}
 
   ngOnInit() {
-    this.gateway.getPerformanceList().subscribe((result) => {
-      this.perfomances = result.performances;
+    this.gateway.getPerformanceList().subscribe(({ performances }) => {
+      this.perfomances = performances;
+      this.changeDetector.markForCheck();
     });
   }
-
 }
