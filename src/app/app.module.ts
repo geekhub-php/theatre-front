c70b14ff2b1a58f3f60896fc6781df420d8a3d9d
IlyaBielov <ibioriginalibi@gmail.com>
2019-04-15 11:50:21 +0300

    change direction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import { BoardTrusteesComponent } from './board-trustees/board-trustees.componen
 import { StripHtmlModule } from './shared/pipes/strip-html/strip-html.module';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { NgxGalleryModule } from 'ngx-gallery';
-import { WidgetComponent } from './performance/widget/widget.component';
+import { WidgetComponent } from './widget/widget.component';
 
 @NgModule({
   declarations: [
