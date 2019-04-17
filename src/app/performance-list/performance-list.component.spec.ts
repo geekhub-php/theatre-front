cd564813aa5f4321118c5b82b80dce535f0f4816
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-04-17 19:17:34 +0300

    Imported SpinnerModule.

--- src/app/performance-list/performance-list.component.spec.ts
+++ src/app/performance-list/performance-list.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PerformanceListComponent } from './performance-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SpinnerModule } from '../shared/spinner/spinner.module';
 
 describe('PerformanceListComponent', () => {
   let component: PerformanceListComponent;
@@ -12,7 +13,8 @@ describe('PerformanceListComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ PerformanceListComponent ],
       imports: [ HttpClientModule,
-                  RouterTestingModule ]
+                  RouterTestingModule,
+                  SpinnerModule]
     })
     .compileComponents();
   }));
