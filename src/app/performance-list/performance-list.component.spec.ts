74a412742d710be404825b339582dd1106a53cf6
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-04-17 19:28:37 +0300

    Added StripHtmlModule.

--- src/app/performance-list/performance-list.component.spec.ts
+++ src/app/performance-list/performance-list.component.spec.ts
@@ -4,6 +4,7 @@ import { PerformanceListComponent } from './performance-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SpinnerModule } from '../shared/spinner/spinner.module';
+import { StripHtmlModule } from '../shared/pipes/strip-html/strip-html.module';
 
 describe('PerformanceListComponent', () => {
   let component: PerformanceListComponent;
@@ -14,7 +15,8 @@ describe('PerformanceListComponent', () => {
       declarations: [ PerformanceListComponent ],
       imports: [ HttpClientModule,
                   RouterTestingModule,
-                  SpinnerModule]
+                  SpinnerModule,
+                  StripHtmlModule]
     })
     .compileComponents();
   }));
