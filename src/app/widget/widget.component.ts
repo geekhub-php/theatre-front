71f47ad96cc565f0005d92c0147cdb9ec1a80da2
IlyaBielov <ibioriginalibi@gmail.com>
2019-04-18 11:34:40 +0300

    use locale for date

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GatewayService } from '../core/service/gateway.service';
 import { PerformanceEvent } from '../core/model/widget/PerformanceEvent';
 import { ActivatedRoute } from '@angular/router';
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-widget',
@@ -12,14 +11,12 @@ import * as moment from 'moment';
 export class WidgetComponent implements OnInit {
   listEvents: Array<PerformanceEvent> = [];
   slug: string;
-  moment: moment.Moment = moment();
 
   constructor(private gateway: GatewayService, private router: ActivatedRoute) { }
 
   ngOnInit() {
     if (this.slug !== null) { this.slug = this.router.snapshot.paramMap.get('slug'); }
     this.gateway.getPerformanceEvents(this.slug).subscribe((res) => {
-      this.moment = moment(res.performance_events[0].date_time); // always only one elentemt in array
       this.listEvents = res.performance_events;
     });
   }
