03a2f79dfa1e45542789d69d4d307867e7a5546b
Inna Ivchenko <i_lincoln_a@list.ru>
2019-04-19 09:52:44 +0300

    some fixes

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,6 +3,7 @@ import { NewsItem } from '../core/model/news/NewsItem';
 import { ActivatedRoute } from '@angular/router';
 import { GatewayService } from '../core/service/gateway.service';
 import { LoaderService } from '../shared/spinner/loader.service';
+import { plainToClass } from 'class-transformer';
 
 @Component({
   selector: 'app-article',
@@ -26,7 +27,7 @@ export class ArticleComponent implements OnInit {
     this.loaderService.start('article');
     const slug = this.router.snapshot.paramMap.get('slug');
     this.gateAway.getNewsBySlug(slug).subscribe((res) => {
-        this.item = res.body;
+        this.item = plainToClass(NewsItem, res);
         this.loaderService.stop('article');
       },
       err => this.loaderService.stop('article')
