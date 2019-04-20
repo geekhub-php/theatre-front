47ff8381da3ead4c0b55bf4bdd111b1f8ac50916
Serhii Polishchuk <spolischook@gmail.com>
2019-04-20 21:20:17 +0300

    Update src/app/news/news.component.ts
    Co-Authored-By: InnaLincoln <42704341+InnaLincoln@users.noreply.github.com>

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
-  limit: string;
+  limit = 5;
   page: number;
   locale: string;
   listPost: Array<NewsItem> = [];
