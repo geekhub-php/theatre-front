63cbb47cb6ea5761ea78d0248dca190381e043fb
ArtemKamenskiy <tema_111@ukr.net>
2019-04-21 00:34:09 +0300

    added fix

--- src/app/home-page/slider/slider.component.spec.ts
+++ src/app/home-page/slider/slider.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
 import { SliderComponent } from './slider.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SliderComponent', () => {
   let component: SliderComponent;
@@ -8,7 +10,11 @@ describe('SliderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SliderComponent ]
+      declarations: [ SliderComponent ],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
--- src/app/home-page/slider/slider.component.ts
+++ src/app/home-page/slider/slider.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { GatewayService } from '../../core/service/gateway.service';
 
 @Component({
   selector: 'app-slider',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  sliderList: Array<any>;
+  slideId: number;
+  count = 0;
+  constructor(private gateway: GatewayService) { }
 
   ngOnInit() {
+    this.gateway.getPerformanceEventList().subscribe(res => {
+      this.sliderList = res.performance_events;
+      this.slideId = this.sliderList[this.count].id;
+    });
   }
 
+  next() {
+    if (this.count < this.sliderList.length - 1) {
+      this.count++;
+    } else {
+      this.count = 0;
+    }
+    this.slideId = this.sliderList[this.count].id;
+  }
+
+  prev() {
+    if (this.count < this.sliderList.length) {
+      this.count--;
+    } if (this.count < 0) {
+      this.count = this.sliderList.length - 1;
+    }
+    this.slideId = this.sliderList[this.count].id;
+  }
+
+  slide(idx: number) {
+    this.count = idx;
+    this.slideId = this.sliderList[this.count].id;
+  }
 }
