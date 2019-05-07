e69b968995fd6e6edea33b1e246222604a15992f
Revsky Andrey <andrey.g1362@gmail.com>
2019-05-07 13:33:31 +0300

    fix title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +9,17 @@ import { Meta } from '@angular/platform-browser';
 export class AppComponent {
   title = 'Черкаський драматичний театр імені Т. Г. Шевченка';
 
-  constructor(private meta: Meta) {
+  constructor(private meta: Meta,
+              private titleService: Title) {
     this.meta.addTag({name: 'og:url', content: 'http://theatre-shevchenko.ck.ua'});
     this.meta.addTag({name: 'og:type', content: 'website'});
     this.meta.addTag({name: 'og:title', content: this.title});
     this.meta.addTag({name: 'og:description', content: 'Черкаський академічний музично-драматичний театр імені Тараса Григоровича Шевченка'});
     this.meta.addTag({name: 'og:image', content: 'http://theatre-shevchenko.ck.ua/img/logo.png'});
+    this.setTitle(this.title);
   }
 
+  setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
