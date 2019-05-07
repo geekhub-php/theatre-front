0226bd81da87e27c0f2b753f4e92119145611707
Revsky Andrey <andrey.g1362@gmail.com>
2019-05-07 09:35:53 +0300

    edit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private meta: Meta) {
+    this.meta.addTag({name: 'og:url', content: 'http://theatre-shevchenko.ck.ua'});
+    this.meta.addTag({name: 'og:type', content: 'website'});
+    this.meta.addTag({name: 'og:title', content: 'Черкаський драматичний театр імені Т. Г. Шевченка'});
+    this.meta.addTag({name: 'og:description', content: 'Черкаський академічний музично-драматичний театр імені Тараса Григоровича Шевченка'});
+    this.meta.addTag({
+      name: 'og:image',
+      content: 'https://scontent.fdnk2-1.fna.fbcdn.net/v/t1.0-0/c0.0.369.369a/p370x247/12122823_1672362166338620_1793269158110847143_n.jpg'
+    });
+  }
 }
