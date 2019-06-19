b24f80ad84f430e1f7fee1afed81974e4ee701da
yana-tsulya <yanatsulya@gmail.com>
2019-06-19 11:57:18 +0300

    Title and general meta added, without translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,9 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Черкаський драматичний театр імені Т. Г. Шевченка';
 
   constructor(private meta: Meta,
-              private titleService: Title) {
-    this.setTitle(this.title);
-
-    this.meta.addTags([
-      {property: 'og:title', content: this.title},
-      {property: 'og:image', content: 'http://theatre-shevchenko.ck.ua/img/logo.png'},
-      {property: 'og:url', content: 'http://theatre-shevchenko.ck.ua'},
-      {property: 'og:type', content: 'website'},
-      {property: 'og:description', content: 'Черкаський академічний музично-драматичний театр імені Тараса Григоровича Шевченка'}
-      ]);
-  }
+              private titleService: Title) {}
 
   setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
