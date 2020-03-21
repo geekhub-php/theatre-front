cabd59946e912b0f34b1871b2cf518c517f54486
Serhii Polishchuk <spolischook@gmail.com>
2020-03-21 23:54:33 +0200

    Added basic structure TheaterGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { TheaterGroup } from 'schema-dts';
+import { TheatreGroupEn } from './core/model/schema/TheatreGroupEn';
+import { TheatreGroupUk } from './core/model/schema/TheatreGroupUk';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +10,12 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  schema: TheaterGroup;
   constructor(private meta: Meta,
-              private titleService: Title) {}
+              private titleService: Title,
+              @Inject(LOCALE_ID) public locale: string) {
+      this.schema = 'en-US' === locale ? TheatreGroupEn.map() : TheatreGroupUk.map();
+  }
 
   setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
---
+++ src/app/core/model/schema/TheatreGroupEn.ts
@@ -0,0 +1,31 @@
+import { TheaterGroup, WithContext, Place, GeoCoordinates } from 'schema-dts';
+
+export class TheatreGroupEn {
+  static map(): WithContext<TheaterGroup> {
+    return {
+      '@context': 'https://schema.org',
+      '@type': 'TheaterGroup',
+      logo: '',
+      name: 'Cherkasy theatre',
+      url: '',
+      location: location
+    };
+  }
+}
+
+const geo: WithContext<GeoCoordinates> = {
+  '@context': 'https://schema.org',
+  '@type': 'GeoCoordinates',
+  latitude: 555,
+  longitude: 555,
+  postalCode: '',
+  addressCountry: 'Ukraine',
+  address: '',
+};
+
+const location: WithContext<Place> = {
+  '@context': 'https://schema.org',
+  '@type': 'Place',
+  geo: geo,
+  hasMap: 'https://www.google.com/maps?ll=49.441986,32.061327&z=16&t=m&hl=en-US&gl=UA&mapclient=embed&cid=9899641005179411837',
+};
