5c3c82d4214a85d0cf5f4d3dde0f5e5ba8161076
Serhii Polishchuk <spolischook@gmail.com>
2020-03-22 00:04:24 +0200

    Fixed lint issues

--- src/app/core/model/schema/TheatreGroupEn.ts
+++ src/app/core/model/schema/TheatreGroupEn.ts
@@ -1,18 +1,5 @@
 import { TheaterGroup, WithContext, Place, GeoCoordinates } from 'schema-dts';
 
-export class TheatreGroupEn {
-  static map(): WithContext<TheaterGroup> {
-    return {
-      '@context': 'https://schema.org',
-      '@type': 'TheaterGroup',
-      logo: '',
-      name: 'Cherkasy theatre',
-      url: '',
-      location: location
-    };
-  }
-}
-
 const geo: WithContext<GeoCoordinates> = {
   '@context': 'https://schema.org',
   '@type': 'GeoCoordinates',
@@ -29,3 +16,16 @@ const location: WithContext<Place> = {
   geo: geo,
   hasMap: 'https://www.google.com/maps?ll=49.441986,32.061327&z=16&t=m&hl=en-US&gl=UA&mapclient=embed&cid=9899641005179411837',
 };
+
+export class TheatreGroupEn {
+  static map(): WithContext<TheaterGroup> {
+    return {
+      '@context': 'https://schema.org',
+      '@type': 'TheaterGroup',
+      logo: '',
+      name: 'Cherkasy theatre',
+      url: '',
+      location: location
+    };
+  }
+}
--- src/app/core/model/schema/TheatreGroupUk.ts
+++ src/app/core/model/schema/TheatreGroupUk.ts
@@ -1,18 +1,5 @@
 import { TheaterGroup, WithContext, Place, GeoCoordinates } from 'schema-dts';
 
-export class TheatreGroupUk {
-  static map(): WithContext<TheaterGroup> {
-    return {
-      '@context': 'https://schema.org',
-      '@type': 'TheaterGroup',
-      logo: '',
-      name: 'Черкаський театр',
-      url: '',
-      location: location
-    };
-  }
-}
-
 const geo: WithContext<GeoCoordinates> = {
   '@context': 'https://schema.org',
   '@type': 'GeoCoordinates',
@@ -26,6 +13,19 @@ const geo: WithContext<GeoCoordinates> = {
 const location: WithContext<Place> = {
   '@context': 'https://schema.org',
   '@type': 'Place',
-  hasMap: 'https://www.google.com/maps?ll=49.441986,32.061327&z=16&t=m&hl=en-US&gl=UA&mapclient=embed&cid=9899641005179411837',
   geo: geo,
+  hasMap: 'https://www.google.com/maps?ll=49.441986,32.061327&z=16&t=m&hl=en-US&gl=UA&mapclient=embed&cid=9899641005179411837',
 };
+
+export class TheatreGroupUk {
+  static map(): WithContext<TheaterGroup> {
+    return {
+      '@context': 'https://schema.org',
+      '@type': 'TheaterGroup',
+      logo: '',
+      name: 'Черкаський театр',
+      url: '',
+      location: location
+    };
+  }
+}
