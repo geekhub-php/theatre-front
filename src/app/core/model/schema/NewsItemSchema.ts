efb4ad9cb967840d31ed0d9f1fcf930547fd00d3
Andrey Revsky <Andrey.g1362@gmail.com>
2020-03-27 20:18:37 +0200

    add info

--- src/app/core/model/schema/NewsItemSchema.ts
+++ src/app/core/model/schema/NewsItemSchema.ts
@@ -6,7 +6,13 @@ export class NewsItemSchema {
     return {
       '@context': 'https://schema.org',
       '@type': 'Article',
-      name: newsItem.title
+      name: newsItem.title,
+      image: newsItem.mainPicture.post_big.url,
+      datePublished: newsItem.created_at,
+      dateModified: newsItem.updated_at,
+      publisher: 'newsItem.updated_by',
+      author: 'newsItem.created_by',
+      headline: 'headline'
     };
   }
 }
