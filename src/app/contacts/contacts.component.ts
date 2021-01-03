09f697d95228a08598bfbf72d4bdeae42388d6f4
Serhii Polishchuk <spolischook@gmail.com>
2021-01-03 22:47:10 +0200

    Fixed lint error

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -13,7 +13,8 @@ export class ContactsComponent implements OnInit {
     this.meta.updateTag({ property: 'og:title', content: 'Контакти Черкаського драматичного театру імені Т. Г. Шевченка' });
     this.meta.updateTag({
       property: 'og:description',
-      content: 'Директор, Секретар - +38(0472)36-07-20 | Адміністратор - +38(0472)36-07-18 | Каса - +38(0472)36-07-21 | Eлектронна пошта - theatre.cherkasy@gmail.com | Звертайтеся за адресою - м. Черкаси, бул. Шевченка, 234'
+      content: 'Директор, Секретар - +38(0472)36-07-20 | Адміністратор - +38(0472)36-07-18 ' +
+        '| Каса - +38(0472)36-07-21 | Eлектронна пошта - theatre.cherkasy@gmail.com | Звертайтеся за адресою - м. Черкаси, бул. Шевченка, 234'
     });
     this.meta.updateTag({property: 'og:image', content: 'http://theatre-shevchenko.ck.ua/assets/images/logo.png'});
   }
