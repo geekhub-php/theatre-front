00bf87a132749bb7c298f283bb82057e2ff676f1
yana-tsulya <yanatsulya@gmail.com>
2021-01-09 14:44:39 +0200

    Fix LangService tests

--- src/app/core/services/lang.service.spec.ts
+++ src/app/core/services/lang.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LangService } from './lang.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LangService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [LangService],
+      imports: [
+        RouterTestingModule.withRoutes([])]
+    });
+  });
 
   it('should be created', () => {
     const service: LangService = TestBed.get(LangService);
