dfe3455f740a4b856e0d310319f3a07ce0d5d9dd
Ilya Bielov <37895784+IlyaBielov@users.noreply.github.com>
2021-01-16 14:04:56 +0200

    implements Google Analytics (#159)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'http://theatre.pp.ua/api',
-  canonicalUrl: 'http://theatre-shevchenko.ck.ua/'
+  canonicalUrl: 'http://theatre-shevchenko.ck.ua/',
+  ga: 'UA-65733857-1'
 };
