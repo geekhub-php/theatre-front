8fccc6cb40ba86fc36ce244164673698489598d5
IlyaBielov <ibioriginalibi@gmail.com>
2021-01-16 15:52:06 +0200

    implements seasons for one performance

--- src/app/seasons/seasons.component.ts
+++ src/app/seasons/seasons.component.ts
@@ -1,19 +1,29 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { LoaderService } from '../shared/spinner/loader.service';
 import { GatewayService } from '../core/services/gateway.service';
 import { Season } from '../core/model/season/Season';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Performance } from '../core/model/performance/Performance';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-seasons',
   templateUrl: './seasons.component.html',
   styleUrls: ['./seasons.component.scss']
 })
-export class SeasonsComponent implements OnInit {
+export class SeasonsComponent implements OnInit, OnDestroy {
+  @Input() performance: Performance = null;
+  sub$: Subscription = new Subscription();
+
   seasons: Array<Season>;
   activeSeasonNumber: number;
   ALL_SEASONS = -1;
   ARCHIVE_SEASON = -2;
+
+  activeAudience: 'kids' | 'adults' | null;
+  ALL_AUDIENCE = null;
+  KIDS_AUDIENCE: 'kids' = 'kids';
+  ADULT_AUDIENCE: 'adults' = 'adults';
 
   constructor(
     private changeDetector: ChangeDetectorRef,
@@ -27,15 +37,16 @@ export class SeasonsComponent implements OnInit {
   ngOnInit() {
     this.loaderService.start('seasons');
 
-    this.activatedRoute.queryParams.subscribe(params => {
-      this.activeSeasonNumber = params['season'] ? +params['season'] : this.activeSeasonNumber;
-    });
-    this.gatewayService.getSeasons().subscribe(seasons => {
-      this.activeSeasonNumber = this.activeSeasonNumber ? this.activeSeasonNumber : seasons[0].number;
-      this.seasons = seasons;
-      this.changeDetector.markForCheck();
-      this.loaderService.stop('seasons');
-      });
+    this.sub$.add(this.activatedRoute.queryParams
+      .subscribe(params => this.activeSeasonNumber = params['season'] ? +params['season'] : this.activeSeasonNumber));
+
+    this.sub$.add(this.gatewayService.getSeasons()
+      .subscribe(seasons => {
+        this.activeSeasonNumber = this.activeSeasonNumber ? this.activeSeasonNumber : seasons[0].number;
+        this.seasons = seasons;
+        this.changeDetector.markForCheck();
+        this.loaderService.stop('seasons');
+      }));
   }
 
   loadSeason(seasonNumber: number) {
@@ -44,5 +55,17 @@ export class SeasonsComponent implements OnInit {
       relativeTo: this.activatedRoute,
       queryParams: {season: seasonNumber}
     });
+  }
+
+  filterAudience(audienceType: 'kids' | 'adults' | null) {
+    this.activeAudience = audienceType;
+  }
+
+  filterSeasonsByPerformance(seasons: Array<Season>, performance: Performance) {
+    return [...seasons].filter((season) => performance.seasons.find((prSeason) => season.number === prSeason.number));
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
   }
 }
