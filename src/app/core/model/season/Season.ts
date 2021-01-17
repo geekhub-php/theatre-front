f21487ab8ff1ca85434f21c3758e81e5498b777d
Ilya Bielov <37895784+IlyaBielov@users.noreply.github.com>
2021-01-17 15:13:27 +0200

    implements seasons for one performance and audience filter (#161)
    * implements seasons for one performance
    
    * fix translate
    
    * fix seasons
    
    * implements filter by audience
    
    * fix audience feature
    
    * update test

--- src/app/core/model/season/Season.ts
+++ src/app/core/model/season/Season.ts
@@ -1,7 +1,10 @@
+import { EAudience } from '../Audience';
+
 export class Season {
   id: number;
   name: string;
   number: number;
+  audience: EAudience;
   start_date: string;
   end_date: string;
   performance_count: number;
