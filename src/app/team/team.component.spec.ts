8f98f94323b36fd7d575836ade794a236577caf1
Serhii Polishchuk <spolischook@gmail.com>
2021-01-18 04:55:25 +0200

    Fixed tests

--- src/app/team/team.component.spec.ts
+++ src/app/team/team.component.spec.ts
@@ -5,6 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { SpinnerModule } from '../shared/spinner/spinner.module';
+import { PersonComponent } from '../person/person.component';
+import { PersonRolesComponent } from '../person-roles/person-roles.component';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 describe('TeamComponent', () => {
   let component: TeamComponent;
@@ -12,11 +15,16 @@ describe('TeamComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TeamComponent],
+      declarations: [
+        TeamComponent,
+        PersonComponent,
+        PersonRolesComponent
+      ],
       imports: [
         HttpClientModule,
         RouterTestingModule,
         InfiniteScrollModule,
+        NgxGalleryModule,
         SpinnerModule
       ]
     })
