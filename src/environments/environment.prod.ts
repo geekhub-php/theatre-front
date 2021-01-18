39321ce3b8ec76ca3b1bf277ce59a39b1cc330a3
Serhii Polishchuk <spolischook@gmail.com>
2021-01-18 12:51:59 +0200

    Fixed url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://theatre.pp.ua/api',
+  baseUrl: 'https://theatre.pp.ua/api',
   canonicalUrl: 'http://theatre-shevchenko.ck.ua/',
   ga: 'UA-65733857-1'
 };
