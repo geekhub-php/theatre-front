775799c7584682b98690ff087c35eaed54d4d1fd
Serhii Polishchuk <spolischook@gmail.com>
2021-04-14 23:34:16 +0300

    Fixed env vars

--- deploy/build-env.github.sh
+++ deploy/build-env.github.sh
@@ -6,5 +6,5 @@ DOMAIN_PROD="http://theatre-shevchenko.ck.ua"
 DOMAIN_STAGING="http://develop.theatre.pp.ua"
 
 echo "BRANCH=$(echo ${GITHUB_REF#refs/heads/} | tr / -)" >> $GITHUB_ENV
-echo "DOMAIN=${DOMAIN_STAGING}/${BRANCH}/en/" >> $GITHUB_ENV
-echo "BASE_HREF=/${BRANCH}/" >> $GITHUB_ENV
+echo "DOMAIN=${DOMAIN_STAGING}/${env.BRANCH}/en/" >> $GITHUB_ENV
+echo "BASE_HREF=/${env.BRANCH}/" >> $GITHUB_ENV
