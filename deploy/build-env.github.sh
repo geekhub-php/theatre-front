4c2d96471180d3eb94bdba3c70f85b25f183ec8b
Serhii Polishchuk <spolischook@gmail.com>
2021-04-14 22:44:15 +0300

    Try to export env vars

--- deploy/build-env.github.sh
+++ deploy/build-env.github.sh
@@ -5,7 +5,6 @@ set -euo
 ENV=$1
 
 export $(xargs < .env)
-export ENV=$ENV
 export BRANCH="github-linter-2"
 export DOMAIN="http://develop.theatre.pp.ua/github-linter-2/en/"
 export BASE_HREF="/github-linter-2/"
