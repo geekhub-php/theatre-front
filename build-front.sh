687b7c27a764d538f3805cf699a6f804719d5e68
Serhii Polishchuk <spolischook@gmail.com>
2021-04-14 23:49:26 +0300

    Try full deploy

--- build-front.sh
+++ build-front.sh
@@ -5,30 +5,30 @@ set -euo
 rm -rf dist
 TMPDIR=$(mktemp -d)
 
-#ng build --configuration="${ENV}-uk" \
-#  --deploy-url="${DOMAIN}/uk/" \
-#  --base-href="${BASE_HREF}uk/" \
-#  --aot true --vendor-chunk true \
-#  --output-path 'dist/' \
-#  --i18n-file src/assets/locale/locale.uk-ua.xlf \
-#  --i18n-format xlf \
-#  --i18n-locale uk-UA \
-#  --verbose \
-#  --i18n-missing-translation=error
-#
-#mv ./dist/uk "${TMPDIR}/"
+ng build --configuration="${ENV}-uk" \
+  --deploy-url="${DOMAIN}uk/" \
+  --base-href="${BASE_HREF}" \
+  --aot true --vendor-chunk true \
+  --output-path 'dist/' \
+  --i18n-file src/assets/locale/locale.uk-ua.xlf \
+  --i18n-format xlf \
+  --i18n-locale uk-UA \
+  --verbose \
+  --i18n-missing-translation=error
+
+mv ./dist/uk "${TMPDIR}/"
 
 ng build --configuration="${ENV}-en" \
-  --deploy-url="${DOMAIN}" \
+  --deploy-url="${DOMAIN}en/" \
   --base-href="${BASE_HREF}" \
   --aot true \
   --vendor-chunk true \
   --output-path "dist/"
 
-#mv "${TMPDIR}/uk" ./dist/
+mv "${TMPDIR}/uk" ./dist/
 rm -rf "${TMPDIR}"
 
 
 echo "Base href: ${BASE_HREF}"
 echo "Domain: ${DOMAIN}"
-echo "Environment: ${ENV}-en"
+echo "Environment: ${ENV}"
--- deploy/build-env.github.sh
+++ deploy/build-env.github.sh
@@ -7,6 +7,6 @@ DOMAIN_STAGING="http://develop.theatre.pp.ua"
 
 BRANCH=$(echo ${GITHUB_REF#refs/heads/} | tr / -)
 echo "BRANCH=$BRANCH" >> $GITHUB_ENV
-echo "DOMAIN=${DOMAIN_STAGING}/${BRANCH}/en/" >> $GITHUB_ENV
+echo "DOMAIN=${DOMAIN_STAGING}/${BRANCH}/" >> $GITHUB_ENV
 echo "BASE_HREF=/${BRANCH}/" >> $GITHUB_ENV
 echo "ENV=staging" >> $GITHUB_ENV
