34221221ce70dc779ab4b584421efdbac1819b8e
Serhii Polishchuk <spolischook@gmail.com>
2021-04-14 23:20:35 +0300

    Try to change github env

--- deploy/build-env.github.sh
+++ deploy/build-env.github.sh
@@ -4,6 +4,7 @@ set -euo
 
 ENV=$1
 
+echo "action_state=yellow" >> $GITHUB_ENV
 export $(xargs < .env)
 export BRANCH="github-linter-2"
 export DOMAIN="http://develop.theatre.pp.ua/github-linter-2/en/"
