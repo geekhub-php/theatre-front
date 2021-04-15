c2014a60b326cbcbcc71dd22f5f698885776334c
Nataliia Koval <hakunata@gmail.com>
2021-04-15 10:53:27 +0300

    change default img, add alt attributes, responsive columns

--- src/app/components/partials/news-list-item/news-list-item.component.ts
+++ src/app/components/partials/news-list-item/news-list-item.component.ts
@@ -9,7 +9,7 @@ import { GatewayService } from '../../../services/gateway.service';
 })
 export class NewsListItemComponent implements OnInit {
   @Input() item: NewsItem;
-  defaultImg = '../../assets/images/cats.gif';
+  defaultImg = '../../assets/images/news/mask.jpg';
 
   constructor(private gatewayService: GatewayService) {
   }
