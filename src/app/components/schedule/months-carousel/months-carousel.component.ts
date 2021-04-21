e79e39fef31cf5f6feded76cb2ed50b8ad6e7d62
mAndrushchenko <andr.mv97@gamil.com>
2021-04-21 08:25:40 +0300

    slider of months modified

--- src/app/components/schedule/months-carousel/months-carousel.component.ts
+++ src/app/components/schedule/months-carousel/months-carousel.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  ViewChild,
+  Inject,
+  PLATFORM_ID,
+  HostListener
+} from '@angular/core';
 import { NguCarouselConfig } from "@ngu/carousel";
 
 @Component({
@@ -6,66 +14,166 @@ import { NguCarouselConfig } from "@ngu/carousel";
   templateUrl: './months-carousel.component.html',
   styleUrls: ['./months-carousel.component.scss']
 })
-export class MonthsCarouselComponent implements OnInit {
-  images = [
+export class MonthsCarouselComponent implements AfterViewInit {
+  @Input() onNextMonth;
+  @Input() onPrevMonth;
+  @Input() currentMonth;
+
+  @ViewChild('myCarousel') carousel;
+
+  public months = [
+    'Листопад',
+    'Грудень',
     'Січень',
     'Лютий',
     'Березень',
     'Квітень',
     'Травень',
-    'Черевень',
+    'Червень',
     'Липень',
     'Серпень',
     'Вересень',
     'Жовтень',
     'Листопад',
-    'Грудень'
+    'Грудень',
+    'Січень',
+    'Лютий',
   ];
-  public carouselTileItems: Array<any> = [0, 1, 2, 3, 4, 5];
-  public carouselTiles = {
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
-    9: [],
-    10: [],
-    11: [],
-  };
+
+  public monthsEng = [
+    'November',
+    'December',
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+    'January',
+    'February',
+  ];
+
+  public currentSize: string;
+  public currentIndex: number;
+  public currentNumberOfMonth: number;
+
+
+  public selectMonth(index: number) {
+    index = this.currentSize === 'xl' ? index - 2 : index - 1;
+
+    let difference = index - this.currentIndex;
+    let count = 0;
+    if (difference > 0) {
+      difference = Math.abs(difference);
+      do {
+        this.onNext();
+        count++;
+      } while (count < difference);
+    } else if (difference < 0) {
+      difference = Math.abs(difference);
+      do {
+        this.onPrev();
+        count++;
+      } while (count < difference);
+    }
+
+    if (index === -1) index = 11;
+    if (index === -2) index = 10;
+    if (index === 12) index = 0;
+    if (index === 13) index = 1;
+
+    this.currentIndex = index;
+
+    return index;
+  }
+
+  public activeMonth(index: number) {
+    if (this.currentSize === 'xl') return index === (this.currentIndex + 2);
+    return index === (this.currentIndex + 1);
+  }
+
   public carouselTile: NguCarouselConfig = {
-    grid: { xs: 5, sm: 5, md: 5, lg: 5, xl:5, all:0 },
+    grid: { xs: 3, sm: 3, md: 3, lg: 3, xl: 5, all: 0 },
     slide: 1,
     speed: 250,
     point: {
       visible: true,
     },
-    load: 2,
-      velocity: 0,
-    touch: true,
+    load: 10,
+    velocity: 0,
+    touch: false,
     easing: 'cubic-bezier(0, 0, 0.2, 1)',
     loop: true,
   };
-  constructor() {}
 
-  ngOnInit() {
-    this.carouselTileItems.forEach(el => {
-      this.carouselTileLoad(el);
-    });
+  constructor(@Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.onResize();
   }
 
-  public carouselTileLoad(j) {
-    // console.log(this.carouselTiles[j]);
-    const len = this.carouselTiles[j].length;
-    if (len <= 30) {
-      for (let i = len; i < len + 15; i++) {
-        this.carouselTiles[j].push(
-          this.images[Math.floor(Math.random() * this.images.length)]
-        );
-      }
+  @HostListener('window:resize') onResize() {
+    const innerWidth = window.innerWidth;
+    const xlBreakPoint = 1200;
+    const mdBreakPoint = 767;
+
+    if ((innerWidth >= xlBreakPoint) && !(this.currentSize === 'xl')) {
+      this.currentSize = 'xl';
+      this.setIndexes(1);
+    } else if ((innerWidth >= mdBreakPoint && innerWidth < xlBreakPoint) && !(this.currentSize === 'md')) {
+      this.currentSize = 'md';
+      this.setIndexes(0);
     }
   }
 
+  onNext() {
+    this.onNextMonth();
+  }
+
+  onPrev() {
+    this.onPrevMonth();
+  }
+
+  onMove(activePoint, carousel) {
+    this.carousel = carousel;
+    this.currentIndex = activePoint;
+  }
+
+  // setStartIndex(size) {
+  //   console.log(size);
+  //   switch (size) {
+  //     case 'xl' :
+  //       this.currentIndex = 0;
+  //       break;
+  //     case 'md' :
+  //       this.currentIndex = 0;
+  //       break;
+  //     default:
+  //       this.currentIndex = 0;
+  //       break;
+  //   }
+  // }
+
+  ngOnInit() {
+  }
+
+  setIndexes(positionDependency: number) {
+    this.currentNumberOfMonth = Number(this.currentMonth) - positionDependency;
+    this.currentIndex = this.currentNumberOfMonth;
+    setTimeout(() => {
+      this.carousel.moveTo(this.currentNumberOfMonth);
+    });
+  }
+
+  ngAfterViewInit() {
+    //preventing After Init Error
+    setTimeout(() => {
+      this.setIndexes(1);
+    });
+  }
 }
