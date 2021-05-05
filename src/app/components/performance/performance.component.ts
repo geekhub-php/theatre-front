9fefbc6109991b21705f183fcdb8f4c55bacdef8
Nataliia Koval <hakunata@gmail.com>
2021-05-05 10:46:15 +0300

    delete console.log, add translation for title, add missing whitespaces in interpolation

--- src/app/components/performance/performance.component.ts
+++ src/app/components/performance/performance.component.ts
@@ -62,7 +62,6 @@ export class PerformanceComponent implements OnInit {
       this.gateway.getPerformanceBySlug(slug),
       this.gateway.getPerformanceRoles(slug)
     ]).subscribe(([performance, roles]) => {
-      console.log('performance', performance);
       this.performance = performance.body;
       this.roles = roles;
       if (this.performance.gallery) {
