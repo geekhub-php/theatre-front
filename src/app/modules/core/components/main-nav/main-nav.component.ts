b989c75e1ae898af1eb583bff3f8addacd2b7a33
Tetiana Dendemarchenko <tetianadendemarchenko@gmail.com>
2021-05-06 04:12:58 +0300

    Removed breadcrumb element

--- src/app/modules/core/components/main-nav/main-nav.component.ts
+++ src/app/modules/core/components/main-nav/main-nav.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { LangService } from '../../../../services/lang.service';
-import { BreadcrumbService } from '../../../../services/breadcrumb.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -9,23 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent {
-  private breakInService: BreadcrumbService;
-  private router: Router;
 
   get langRedirectUrl() {
     return this.langService.getLangRedirectUrl();
   }
 
   constructor(private langService: LangService) {
-  }
-
-  breakIn(breakInTarget: string): void {
-    this.breakInService.breakIn(breakInTarget);
-    this.router.navigate(['/']);
-  }
-
-  resetbreakIn(): void {
-    this.breakInService.resetBreakIns();
-    this.router.navigate(['/']);
   }
 }
