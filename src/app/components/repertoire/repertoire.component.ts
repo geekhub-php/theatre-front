f55ae4013d4fc0fd88fcf1dfcc94b5f34c30fca1
Mykhailo Andrushchenko <andr.mv97@gmail.com>
2021-07-04 17:49:13 +0300

    Styles fixed

--- src/app/components/repertoire/repertoire.component.ts
+++ src/app/components/repertoire/repertoire.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { GatewayService } from '../../services/gateway.service';
@@ -6,10 +6,16 @@ import { PerformanceListResponse } from '../../store/performance/PerformanceList
 import { Performance } from '../../store/performance/Performance';
 import { LoaderService } from '../partials/spinner/loader.service';
 
+enum RepertoireViewModes {
+  DESKTOP = 'Desktop',
+  TABLET = 'Tablet',
+  MOBILE = 'Mobile'
+}
+
 @Component({
   selector: 'app-repertoire',
   templateUrl: './repertoire.component.html',
-  styleUrls: ['./repertoire.component.scss'],
+  styleUrls: [ './repertoire.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RepertoireComponent implements OnInit {
@@ -19,18 +25,30 @@ export class RepertoireComponent implements OnInit {
   collectionSize: number;
 
   response: PerformanceListResponse;
-  perfomances: Array<Performance>;
+  performances: Array<Performance>;
   seasonNumber: number;
   audience: 'kids' | 'adults' | null;
   CURRENT_SEASON = 0; // api conventions to get recent season
   CURRENT_AUDIENCE = null;
 
+  viewMode: null | RepertoireViewModes = null;
+  views = RepertoireViewModes;
+
   constructor(
     private changeDetector: ChangeDetectorRef,
     private gateway: GatewayService,
     private loaderService: LoaderService,
-    private activatedRoute: ActivatedRoute,
-  ) {
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  @HostListener('window:resize', [ '$event' ]) onResize() {
+    const desktop = 1480;
+    const mobile = 768;
+    const { innerWidth } = window;
+    this.viewMode = innerWidth >= desktop ?
+      RepertoireViewModes.DESKTOP : innerWidth < desktop && innerWidth > mobile ?
+      RepertoireViewModes.TABLET :
+      RepertoireViewModes.MOBILE;
   }
 
   ngOnInit() {
@@ -40,10 +58,11 @@ export class RepertoireComponent implements OnInit {
       this.audience = params['audience'] || this.CURRENT_AUDIENCE;
       this.getPerformances(this.seasonNumber, this.audience);
     });
+    this.onResize();
   }
 
   getPerformances(seasonNumber: number, audience: 'kids' | 'adults' | null) {
-    this.perfomances = [];
+    this.performances = [];
     this.loaderService.start('repertoire');
 
     const getSeasonPerformancesSub$ = audience
@@ -52,12 +71,12 @@ export class RepertoireComponent implements OnInit {
 
     getSeasonPerformancesSub$
       .subscribe((performances) => {
-        this.perfomances = performances;
-        this.collectionSize = performances.length;
-        this.changeDetector.markForCheck();
-        this.loaderService.stop('repertoire');
-      },
-      error1 => this.loaderService.stop('repertoire')
-    );
+          this.performances = performances;
+          this.collectionSize = performances.length;
+          this.changeDetector.markForCheck();
+          this.loaderService.stop('repertoire');
+        },
+        error1 => this.loaderService.stop('repertoire')
+      );
   }
 }
