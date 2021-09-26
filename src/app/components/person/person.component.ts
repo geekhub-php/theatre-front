efe7856b87bb776ccd67d8d2ddb225e974f7f438
Mykhailo Andrushchenko <andr.mv97@gmail.com>
2021-09-26 14:56:24 +0300

    DT-45 Fix: blue links

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { plainToClass } from 'class-transformer';
@@ -11,7 +11,10 @@ import { LoaderService } from '../partials/spinner/loader.service';
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.scss']
+  styleUrls: ['./person.component.scss'],
+  // disabled for styling innerHTML code
+  // tslint:disable-next-line:use-component-view-encapsulation
+  encapsulation: ViewEncapsulation.None
 })
 export class PersonComponent implements OnInit {
   @Input() person: Employee;
