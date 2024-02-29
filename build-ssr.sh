28be610a97efcf6980f6b8d89006c38d51ef7222
Serhii Polishchuk <spolischook@gmail.com>
2024-02-29 15:55:13 +0100

    Fixed build-ssr - used local version of ng

--- build-ssr.sh
+++ build-ssr.sh
@@ -6,12 +6,12 @@ set -e
 set -o errexit
 
 rm -rf dist
-ng build -c "$ENV"-uk && ng run theatre-front:server:"$ENV"-uk
+./node_modules/.bin/ng build -c "$ENV"-uk && ./node_modules/.bin/ng run theatre-front:server:"$ENV"-uk
 
 mv dist/theatre-front/browser/uk dist/browser_uk
 mv dist/theatre-front/server/uk dist/server_uk
 
-ng build -c "$ENV"-en && ng run theatre-front:server:"$ENV"-en
+./node_modules/.bin/ng build -c "$ENV"-en && ./node_modules/.bin/ng run theatre-front:server:"$ENV"-en
 
 mv dist/browser_uk dist/theatre-front/browser/uk
 mv dist/server_uk dist/theatre-front/server/uk
